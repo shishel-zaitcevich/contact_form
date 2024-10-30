3d28ecc258ba6d075f8603d39fbcbaeceee1d1cd
unknown <ushka_ya@mail.ru>
2024-10-31 01:14:20 +0300

    changed: contact.js

--- api/contact.js
+++ api/contact.js
@@ -1,16 +1,23 @@
 module.exports = (req, res) => {
-    if (req.method !== 'POST') {
-      return res.status(405).json({ message: 'Метод не поддерживается' });
+    try {
+        if (req.method !== 'POST') {
+            return res.status(405).json({ message: 'Метод не поддерживается' });
+        }
+
+        const { name, email, message } = req.body;
+
+        if (!name || !email || !message) {
+            return res.status(400).json({ message: 'Все поля обязательны для заполнения.' });
+        }
+
+        console.log(`Получено сообщение от ${name}:`, {
+            email,
+            message,
+        });
+
+        res.status(200).json({ message: `Спасибо за проявленный интерес, ${name}!` });
+    } catch (error) {
+        console.error('Ошибка в API:', error);
+        res.status(500).json({ message: 'Внутренняя ошибка сервера.' });
     }
-  
-    const { name, email, message } = req.body;
-  
-    // данные в консоль
-    console.log(`Получено сообщение от ${name}:`, {
-      email,
-      message,
-    });
-  
-    // ответ с сообщением
-    res.status(200).json({ message: `Спасибо за проявленный интерес, ${name}!` });
-  };
+};
